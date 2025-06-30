1f65bc92c36633ff7194186111a86da3222b1e96
Shankha2003 <pariashankhadeep@gmail.com>
2025-06-30 13:15:10 +0530

    print 1 to 10 by using for loop

---
+++ forLoop.java
@@ -0,0 +1,10 @@
+public class forLoop {
+    public static void main(String[] args) {
+        System.out.println(" print 1 to 10");
+
+        for(int i=1; i<11 ; i++ ){
+            System.out.println(i);
+        }
+    }
+    
+}
