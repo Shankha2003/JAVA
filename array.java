c9603cb910b8edcca78bbf3126463917c5dfe173
Shankha2003 <pariashankhadeep@gmail.com>
2025-07-02 00:14:57 +0530

    Array

---
+++ array.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class array {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+
+        marks[0] = 95;
+        marks[1] = 97;
+        marks[2] = 99;
+
+        for(int i = 0; i < 3; i++){
+              System.out.print("Obtained marks :" + marks[i]);
+        }
+    }
+    
+}
