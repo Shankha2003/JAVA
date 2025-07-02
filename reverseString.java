5e075a45bcf841608362fb3a7e29e425df983b8a
Shankha2003 <pariashankhadeep@gmail.com>
2025-07-03 00:21:16 +0530

    Sorting

--- reverseString.java
+++ reverseString.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class reverseString {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder("hello");
+        StringBuilder sb = new StringBuilder("hello"); 
 
         for(int i = 0 ;i < sb.length()/2 ; i++){
             int front = i;
