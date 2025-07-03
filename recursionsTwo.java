d99d4ff8692b170b8cf66c45f069fb2624f0f7fd
Shankha2003 <pariashankhadeep@gmail.com>
2025-07-03 19:16:39 +0530

    Recursion part 1

---
+++ recursionsTwo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class recursionsTwo {
+
+    public static void printNumb(int n){
+        if(n == 6){
+            return;
+        }
+        
+        System.out.println(n);
+        printNumb(n+1);
+    }
+
+
+    public static void main(String[] args) {
+        int n = 1;
+        printNumb(n);
+    }
+    
+}
